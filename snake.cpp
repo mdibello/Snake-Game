43188eaedd8af834703a43c3e09655e62e1f5137
Matthew DiBello <matthew.quickbeam@gmail.com>
2016-11-30 21:50:00 -0500

    Some more robust basic rendering

--- snake.cpp
+++ snake.cpp
@@ -3,16 +3,27 @@
 
 #include <iostream>
 #include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
 
 using namespace std;
 
+const int GRID_WIDTH = 10;
+const int SCREEN_WIDTH = 900;
+
+void render(sf::RenderWindow& window);
+
 int main() {
 	
-	sf::Window window(sf::VideoMode(800, 600), "My Window");
+	sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_WIDTH), 
+		"My Window", sf::Style::Titlebar | sf::Style::Close);
 	// sync application refresh rate with monitor
 	window.setVerticalSyncEnabled(true);
 	// limit refresh rate to 60 fps
 	window.setFramerateLimit(60);
+	window.setKeyRepeatEnabled(false);
+
+	window.clear(sf::Color(0, 0, 255));
+	window.display();
 	
 	while (window.isOpen()) {
 		sf::Event event;
@@ -28,11 +39,11 @@ int main() {
 				case sf::Event::LostFocus:
 					// pause?
 					break;
-				case sf::Event::LostFocus:
+				case sf::Event::GainedFocus:
 					// resume?
 					break;
 				case sf::Event::KeyPressed:
-					// something
+					render(window);
 					break;
 				default:
 					break;
@@ -42,5 +53,14 @@ int main() {
 	}
 
 	return 0;
+
+}
+
+void render(sf::RenderWindow& window) {
+
+	window.clear(sf::Color::Black);
+
+	window.display();
+
 
 }
